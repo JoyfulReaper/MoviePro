ede6c58cab07ba6fa16b575018d8f8ec0bd92aa5
Kyle Givler <xvegan88x@gmail.com>
2021-11-26 15:46:40 -0500

    CF Code along

---
+++ MoviePro/Controllers/ActorsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviePro.Services.Interfaces;
+using System.Threading.Tasks;
+
+namespace MoviePro.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly IRemoteMovieService _movieService;
+        private readonly IDataMappingService _dataMapping;
+
+        public ActorsController(IRemoteMovieService movieService,
+            IDataMappingService dataMapping)
+        {
+            _movieService = movieService;
+            _dataMapping = dataMapping;
+        }
+
+        public async Task<IActionResult> Detail(int id)
+        {
+            var actor = await _movieService.ActorDetailAsync(id);
+            actor = _dataMapping.MapActorDetail(actor);
+
+            return View(actor);
+        }
+    }
+}
