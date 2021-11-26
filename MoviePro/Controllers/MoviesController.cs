3ffc76a8d6060b8512a6850854370e1f22e97efa
Kyle Givler <xvegan88x@gmail.com>
2021-11-26 11:38:44 -0500

    CF Code along

--- MoviePro/Controllers/MoviesController.cs
+++ MoviePro/Controllers/MoviesController.cs
@@ -191,6 +191,36 @@ namespace MoviePro.Controllers
             return _context.Movie.Any(e => e.Id == id);
         }
 
+        public async Task<IActionResult> Details(int? id, bool local = false)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            Movie movie = new();
+            if(local)
+            {
+                movie = await _context.Movie
+                    .Include(m => m.Cast)
+                    .Include(m => m.Crew)
+                    .FirstOrDefaultAsync(m => m.Id == id);
+            }
+            else
+            {
+                var movieDetail = await _movieService.MovieDetailAsync(id.Value);
+                movie = await _mappingService.MapMovieDetailAsync(movieDetail);
+            }
+
+            if(movie == null)
+            {
+                return NotFound();
+            }
+
+            ViewData["Local"] = local;
+            return View(movie);
+        }
+
         private async Task AddToMovieCollection(int movieId, string collectionName)
         {
             var collection = await _context.Collection.FirstOrDefaultAsync(c => c.Name == collectionName);
