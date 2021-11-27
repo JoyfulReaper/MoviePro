a1c61b7eac3f8c9cfa80b3c36a01b5ff1da84e2f
Kyle Givler <xvegan88x@gmail.com>
2021-11-27 16:39:41 -0500

    CF Code along

--- MoviePro/Startup.cs
+++ MoviePro/Startup.cs
@@ -50,6 +50,7 @@ namespace MoviePro
             services.AddTransient<SeedService>();
             services.AddScoped<IRemoteMovieService, TMDBMovieService>();
             services.AddSingleton<IImageService, BasicImageService>();
+            services.AddScoped<IDataMappingService, TMDBMappingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
